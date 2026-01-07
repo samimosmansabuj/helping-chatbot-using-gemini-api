13861fcfcc0e57a9cb0b956ddb8ca16b09290c3f
Samim Osman Sabuj <samim.o.sabuj01@gmail.com>
2026-01-08 03:38:13 +0600

    credentials update!!!

--- not_main.py
+++ not_main.py
@@ -1,10 +1,13 @@
 from google import genai
+from app.config import GEMINI_API_KEY
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-client = genai.Client(api_key="AIzaSyALEJgn6XgHKobYwSXu3rdCMW2dlyRHeTU")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash-lite", contents="Explain how AI works in a few words"
-    # model="gemini-2.5-flash", contents="Explain how AI works in a few words"
 )
 print(response)
 print("================")
