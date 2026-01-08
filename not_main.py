a29a738d5f278408d94afd8a403a6b2eea0474aa
Samim Osman Sabuj <samim.o.sabuj01@gmail.com>
2026-01-09 04:15:00 +0600

    update credential env!!!

--- not_main.py
+++ not_main.py
@@ -1,14 +1,22 @@
-from google import genai
+# from google import genai
+# from app.config import GEMINI_API_KEY
+
+# client = genai.Client(api_key=GEMINI_API_KEY)
+
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash-lite", contents="Explain how AI works in a few words"
+# )
+# print(response)
+# print("================")
+# print(response.text)
+
+import google.generativeai as genai
 from app.config import GEMINI_API_KEY
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
-client = genai.Client(api_key=GEMINI_API_KEY)
+genai.configure(GEMINI_API_KEY)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash-lite", contents="Explain how AI works in a few words"
-)
-print(response)
-print("================")
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+response = model.generate_content("Artificial Intelligence Furure?")
 print(response.text)
+
